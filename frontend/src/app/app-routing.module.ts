b49eb05f96770c446837f813db454280ca724c5f
S00201014-ATU <S00201014@atu.ie>
2024-08-24 22:42:55 +0100

    Add route for Checkout Component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products/:id', component:ProductDetailComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'checkout', component:CheckoutComponent },
 ];
 
 @NgModule({
