78a1f1c4db45f92aa198f9783392b7e7a5cf289c
S00201014-ATU <S00201014@atu.ie>
2024-08-24 19:58:42 +0100

    Generate Cart Component for displaying products in the cart

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NavigationBarComponent } from './components/navigation-bar/navigation-b
     ProductListComponent,
     ProductDetailComponent,
     NavigationBarComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
