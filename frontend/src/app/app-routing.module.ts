19e1cbd994a3737c42c61ae351d36c87800fc1b0
S00201014-ATU <S00201014@atu.ie>
2024-08-24 16:09:47 +0100

    Add route for Product Detail Component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
+  { path: 'products/:id', component:ProductDetailComponent },
 ];
 
 @NgModule({
