7ec172f70cc63bb25fafeaebbd4716e00febd240
S00201014-ATU <S00201014@atu.ie>
2024-08-24 16:06:40 +0100

    Generate Product Detail Component for displaying individual product details

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
