10c89cbfccd2d3c19561e54978ec1202836aeb18
S00201014-ATU <S00201014@atu.ie>
2024-08-24 20:11:43 +0100

    Add functionality to add products to cart from Product Detail Component

--- frontend/src/app/components/product-detail/product-detail.component.ts
+++ frontend/src/app/components/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,7 +12,8 @@ export class ProductDetailComponent implements OnInit{
 
   product: any;
 
-  constructor(private route:ActivatedRoute, private productService: ProductService){}
+  constructor(private route:ActivatedRoute, private productService: ProductService,
+    private cartService:CartService ){}
 
   ngOnInit(): void {
     //Get product ID from route
@@ -19,6 +21,11 @@ export class ProductDetailComponent implements OnInit{
     this.productService.getProducts().subscribe((products) => {
       //Find the product by ID
       this.product = products.find((product) => product._id === productId);
-    })
+    });
+  }
+
+  addToCart(product: any): void {
+    this.cartService.addToCart(product);
+    alert(`${product.name} has been added to the cart!`);
   }
 }
