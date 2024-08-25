45ee7012f3c062c77b85653a825d05efd38789c6
S00201014-ATU <S00201014@atu.ie>
2024-08-25 08:43:33 +0100

    Got product delete function working.

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -60,4 +60,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+//DELETE request to delete a product
+router.delete('/:id', async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+    
+    if (deletedProduct) {
+      res.status(200).json({ message: 'Product deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete product', error: err });
+  }
+});
+
 module.exports = router;
--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +10,36 @@ import { ProductService } from '../../services/product.service';
 export class ProductListComponent implements OnInit {
   products: any[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private http:HttpClient) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((data) => {
-      this.products = data;
+    // More efficient way of loading in products
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.http.get('http://localhost:3000/api/products').subscribe((response: any) => {
+      this.products = response;
     });
+  }
+
+  confirmDelete(productId: string): void {
+    const confirmDelete = window.confirm('Are you sure you want to delete this product?');
+    if (confirmDelete) {
+      this.deleteProduct(productId);
+    }
+  }
+
+  deleteProduct(productId: string): void {
+    this.http.delete(`http://localhost:3000/api/products/${productId}`).subscribe(
+      () => {
+        alert('Product deleted successfully!');
+        this.loadProducts();
+      },
+      (error) => {
+        alert('Failed to delete product');
+        console.error(error);
+      }
+    )
   }
 }
