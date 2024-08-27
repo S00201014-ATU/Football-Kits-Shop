744b09fb825cbed68484672ac700a59de4de75cf
S00201014-ATU <S00201014@atu.ie>
2024-08-27 17:41:30 +0100

    Product related functions working through render now. Trying to get users working atm

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// Other requires
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -7,20 +8,18 @@ const productsRoute = require('./routes/products');
 const usersRoute = require('./routes/users');
 const ordersRoute = require('./routes/orders');
 const checkoutRoute = require('./routes/checkout');
-const cartRoute =  require('./routes/cart');
+const cartRoute = require('./routes/cart');
 const app = express();
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Use CORS to allow requests from different origins
-//Will use netlfiy to run frontend
 app.use(cors({
   origin: ['http://localhost:4200', 'https://your-netlify-domain.netlify.app', 'https://football-kits-shop-kveq.onrender.com'],
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
 }));
-
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -33,13 +32,17 @@ mongoose.connect(mongoUri, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-// Routes
+// Add logging for the /api/users route to confirm if it gets hit
+app.use('/api/users', (req, res, next) => {
+  console.log('Hit /api/users route');
+  next();
+}, usersRoute);
+
+// Routes for products, orders, checkout, and cart
 app.use('/api/products', productsRoute);
-app.use('/api/users', usersRoute);
 app.use('/api/orders', ordersRoute);
 app.use('/api/checkout', checkoutRoute);
 app.use('/api/cart', cartRoute);
-
 
 // Handle the root route
 app.get('/', (req, res) => {
